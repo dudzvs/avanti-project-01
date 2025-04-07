1581af16ab3e6cbf33b29406aa5cf9c4213f2077
dudz <dzvcs@proton.me>
2025-04-07 14:15:54 -0300

    add modal and create functionality to show the searched word

--- src/main.js
+++ src/main.js
@@ -1,3 +1,23 @@
 import '../src/styles/style.scss';
 import * as bootstrap from 'bootstrap';
 import 'bootstrap-icons/font/bootstrap-icons.css';
+
+document.addEventListener('DOMContentLoaded', () => {
+    const forms = document.querySelectorAll('form');
+    const searchedWordContainer = document.getElementById('searchedWord');
+
+    forms.forEach((form) => {
+        form.addEventListener('submit', (e) => {
+            e.preventDefault();
+
+            const input = form.querySelector('input');
+            const value = input.value;
+            searchedWordContainer.textContent = value;
+            const modalTrigger = form.querySelector("[data-bs-toggle='modal']"); // Botão dentro do form
+
+            if (modalTrigger) {
+                modalTrigger.click();
+            }
+        });
+    });
+});
